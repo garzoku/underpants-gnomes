ac25cc816b5b9c368407b63c0518cfeeaf763764
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-04-04 16:20:09 -0500

    complete exercise

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function displayPlan(){
+function displayPlan() {
   const step1 = "Collect underpants."
   const step2 = "???"
   const step3 = "Profit!"
+  const $lis = document.querySelectorAll("li")
+
+  $lis[0].textContent = step1
+  $lis[1].textContent = step2
+  $lis[2].textContent = step3
+
+  console.log("h")
 }
